1a35cea686640620924573cb2ad3983388ad7954
Eric Wang <yaozu.wang@outlook.com>
2017-10-11 10:49:44 +1100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { MockProvider } from '../providers/provider';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    MockProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MockProvider} from '../../providers/provider';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: string[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private mockProvider: MockProvider) {
+    this.items = mockProvider.getData();
   }
 
+  doInfinite(infiniteScroll) {
+    this.mockProvider.getAsyncData().then((newData) => {
+      for (var i = 0; i < newData.length; i++) {
+        this.items.unshift( newData[i] );
+      }
+
+      infiniteScroll.complete();
+
+      if (this.items.length > 90) {
+        infiniteScroll.enable(false);
+      }
+    });
+  }
 }
